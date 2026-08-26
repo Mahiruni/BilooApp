b649f81f42a743dab838b922b04a7fdca6576c10
Biloo <102407996+Mahiruni@users.noreply.github.com>
2026-08-26 05:20:50 -1200

    Add Flutter app library entrypoint

---
+++ mobile/lib/main.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'core.dart';
+part 'auth.dart';
+part 'home.dart';
+part 'explore.dart';
+part 'commerce.dart';
